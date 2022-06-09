08d44e37a7dcfd6215f4596a13df1c1468e493a4
intervisionlord <ads-net@mail.ru>
2022-06-09 19:50:49 +0300

    init

---
+++ vms-killer-WIN.py
@@ -0,0 +1,14 @@
+import psutil
+
+VMS = r'TS VMS 2.0.exe'
+VMSSERV = r'TS VMS Server.exe'
+
+def killServer(VMS, VMSSERV):
+    for proc in psutil.process_iter():
+        if proc.name() == VMS:
+            print('TS VMS is still running!')
+            exit(0)
+        elif proc.name() == VMSSERV or proc.name() == 'vms_crash_handler_x64.exe' or proc.name() == 'Watcher.exe':
+            proc.terminate()
+
+killServer(VMS, VMSSERV)
