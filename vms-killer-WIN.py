f1b29eef8b7f1dc587acf9ab33eb8e202f7a8148
intervisionlord <ads-net@mail.ru>
2022-06-14 15:22:01 +0300

    Незначительные правки  + Миграция на VSCode, CRLF-LF

--- vms-killer-WIN.py
+++ vms-killer-WIN.py
@@ -7,8 +7,10 @@ def killServer(VMS, VMSSERV):
     for proc in psutil.process_iter():
         if proc.name() == VMS:
             print('TS VMS is still running!')
-            exit(0)
+            exit(1)
         elif proc.name() == VMSSERV or proc.name() == 'vms_crash_handler_x64.exe' or proc.name() == 'Watcher.exe':
             proc.terminate()
+            print('TS VMS Server and components were killed')
+    print('Done!')
 
 killServer(VMS, VMSSERV)
